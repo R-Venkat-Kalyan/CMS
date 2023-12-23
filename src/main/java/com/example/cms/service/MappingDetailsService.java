df490c858910256218e6265b7907558de39bd073
R-Venkat-Kalyan <kalyanreddy1439@gmail.com>
2023-12-23 09:53:38 +0530

    Updated CMS

---
+++ src/main/java/com/example/cms/service/MappingDetailsService.java
@@ -0,0 +1,33 @@
+package com.example.cms.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.cms.entity.MappingDetails;
+import com.example.cms.repository.MappinngDetailsRepository;
+
+@Service
+public class MappingDetailsService {
+	
+	@Autowired
+	private MappinngDetailsRepository mdr;
+	
+	public void storeToDB(MappingDetails md) {
+		mdr.save(md);
+	}
+
+	public List<MappingDetails> getAllRecords(){
+		return mdr.findAll();
+	}
+	
+	public MappingDetails getById(long id) {
+		return mdr.findById(id).orElse(null);
+	}
+	
+	public void deleteMapping(long id) {
+		mdr.deleteById(id);
+	}
+}
